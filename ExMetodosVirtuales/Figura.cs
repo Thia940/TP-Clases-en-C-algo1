39e5357fcccd797bd3f4747436de3a3f58ba912e
Thia940 <thiagoncls50@gmail.com>
2025-10-20 23:51:10 -0300

    Final

---
+++ ExMetodosVirtuales/Figura.cs
@@ -0,0 +1,135 @@
+﻿//Participantes del grupo: Thiago Scooby Rebora y Emiliano Shaggy Navarretta
+
+using System;
+using System.Drawing;
+
+namespace Figuras
+{
+    public class Figura
+    {
+        public Pen Lapiz { get; set; }
+
+        public virtual void Dibujar(Graphics g, int posX, int posY){
+            //metodo para polimorfismo
+        }
+    }
+
+    public class Rectangulo : Figura
+    {
+        protected int altura;
+        protected int baseX;
+
+        public Rectangulo(int ancho, int alto)
+        {
+            this.baseX = ancho;
+            this.altura = alto;
+        }
+
+        public override void Dibujar(Graphics g, int posX, int posY)
+        {
+            Pen lapizUsar = this.Lapiz ?? new Pen(Color.Green);
+
+            Point[] vertices = new Point[]
+            {
+                new Point(posX, posY),
+                new Point(posX + baseX, posY),
+                new Point(posX + baseX, posY + altura),
+                new Point(posX, posY + altura)
+            };
+
+            g.DrawPolygon(lapizUsar, vertices);
+        }
+    }
+
+    public class Cuadrado : Rectangulo
+    {
+        public Cuadrado(int lado) : base(lado, lado) { }
+
+        public override void Dibujar(Graphics g, int posX, int posY)
+        {
+            Pen lapizUsar = this.Lapiz ?? new Pen(Color.MediumVioletRed);
+
+            Point[] vertices = new Point[]
+            {
+                new Point(posX, posY),
+                new Point(posX + baseX, posY),
+                new Point(posX + baseX, posY + altura),
+                new Point(posX, posY + altura),
+                new Point(posX, posY) // Cierra el polígono
+            };
+
+            g.DrawPolygon(lapizUsar, vertices);
+        }
+    }
+
+    public class Circulo : Figura
+    {
+        private int radio;
+
+        public Circulo(int radio)
+        {
+            this.radio = radio;
+        }
+
+        public override void Dibujar(Graphics g, int posX, int posY)
+        {
+            Pen lapizUsar = this.Lapiz ?? new Pen(Color.Red);
+
+            g.DrawEllipse(lapizUsar, posX, posY, radio, radio);
+        }
+    }
+
+    public class TrianguloIsosceles : Figura
+    {
+        protected int lado;
+        protected int baseTriangulo;
+
+        public TrianguloIsosceles(int lado, int baseTriangulo)
+        {
+            this.lado = lado;
+            this.baseTriangulo = baseTriangulo;
+        }
+
+        public override void Dibujar(Graphics g, int posX, int posY)
+        {
+            Pen lapizUsar = this.Lapiz ?? new Pen(Color.SteelBlue);
+
+            double altura = Math.Sqrt(Math.Pow(lado, 2) - Math.Pow(baseTriangulo / 2.0, 2));
+
+            Point[] vertices = new Point[]
+            {
+                new Point(posX, (int)(posY + altura)),
+                new Point(posX + baseTriangulo / 2, posY),
+                new Point(posX + baseTriangulo, (int)(posY + altura))
+            };
+
+            g.DrawPolygon(lapizUsar, vertices);
+        }
+    }
+
+    public class TrianguloEquilatero : Figura
+    {
+        protected int lado;
+
+        public TrianguloEquilatero(int lado)
+        {
+            this.lado = lado;
+        }
+
+        public override void Dibujar(Graphics g, int posX, int posY)
+        {
+            Pen lapizUsar = this.Lapiz ?? new Pen(Color.MintCream);
+
+            double altura = (Math.Sqrt(3) / 2) * lado;
+
+            Point[] vertices = new Point[]
+            {
+                new Point(posX, (int)(posY + altura)),
+                new Point(posX + lado / 2, posY),
+                new Point(posX + lado, (int)(posY + altura))
+            };
+
+            g.DrawPolygon(lapizUsar, vertices);
+        }
+    }
+}
---
+++ ExMetodosVirtuales/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Figuras
+{
+    public partial class Form1 : Form
+    {
+        // Array que almacenará todas las figuras a dibujar
+        private Figura[] coleccionFiguras;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Crear generador de números aleatorios para colores
+            Random rnd = new Random();
+
+            // Generar colores aleatorios para cada figura
+            Color col1 = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            Color col2 = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            Color col3 = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            Color col4 = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            Color col5 = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+
+            // Crear lápices con grosor 2 para cada figura
+            Pen lapiz1 = new Pen(col1, 2);
+            Pen lapiz2 = new Pen(col2, 2);
+            Pen lapiz3 = new Pen(col3, 2);
+            Pen lapiz4 = new Pen(col4, 2);
+            Pen lapiz5 = new Pen(col5, 2);
+
+            // Inicializar todas las figuras con sus respectivos lápices
+            coleccionFiguras = new Figura[]
+            {
+                new Circulo(20) { Lapiz = lapiz1 },
+                new Rectangulo(60, 90) { Lapiz = lapiz2 },
+                new Cuadrado(100) { Lapiz = lapiz3 },
+                new TrianguloEquilatero(80) { Lapiz = lapiz5 },
+                new TrianguloIsosceles(90, 100) { Lapiz = lapiz4 }
+            };
+        }
+
+        // Evento que se dispara al presionar el botón de dibujar
+        private void btnDibujar_Click(object sender, EventArgs e)
+        {
+            // Obtener objeto Graphics del PictureBox
+            Graphics lienzo = pictureBox1.CreateGraphics();
+
+            // Dibujar cada figura, separadas horizontalmente
+            for (int i = 0; i < coleccionFiguras.Length; i++)
+            {
+                coleccionFiguras[i].Dibujar(lienzo, i * 100, 50);
+            }
+        }
+    }
+}
